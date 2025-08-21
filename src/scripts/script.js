2c0c22bcbd35dceb0e19f6632070f7ef35fef8b7
Mateus Ferreira <mateusjunior803@gmail.com>
2025-08-21 18:06:45 -0300

    :sparkles: feat: Add showStats() function to render user stats
    Implement showStats() to create elements dynamically and display user statistics inside the container.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,4 +10,25 @@ async function getUserStats() {
   }
 }
 
-getUserStats();
+async function showStats() {
+  const categoryWrapper = document.querySelector(".category-scores");
+  const elements = await getUserStats();
+
+  elements.forEach(element => {
+    categoryWrapper.insertAdjacentHTML("beforeend", `
+      <div class="category ${element.category.toLowerCase()}">
+        <div class="content">
+          <img src="${element.icon}" alt=""></img>
+          <span>${element.category}</span>
+        </div>
+        <div class="info">
+          <span>${element.score}</span>
+          <span>/</span>
+          <span>100</span>
+        </div>
+      </div>
+    `)
+  });
+}
+
+showStats();
